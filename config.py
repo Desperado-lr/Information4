581c38666dc365d8fa509a467e0d82e36300e4ad
Desperado-lr <suxi1104@gmail.com>
2018-10-17 23:31:22 +0800

    抽取不同环境下的配置 及 通过配置名字获取指定配置

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
 
     SECRET_KEY = "cFkIZAsQm7CV3tTTYaNO3nVSS2p1Q/rrCFNlBlmptbFvcQn58o/bMgqOhELkdNU5"
 
-     #  为mysql 添加配置
+    #  为mysql 添加配置
     SQLALCHEMY_DATABASE_URI = "mysql://root:123456@127.0.0.1:3306/information4"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -20,8 +20,33 @@ class Config(object):
     # 是否开始session签名
     SESSION_USE_SIGNER = True
     # 定义Session 保存的 redis
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
     # 设置需要过期
     SESSION_PERMANENT = False
     # 设置过期时间
-    PERMANENT_SESSION_LIFETIME =86400 * 2
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
+
+
+class DevelopmentConfig(Config):
+    """开发环境下的配置"""
+    DEBUG = True
+
+
+class PorductionConfig(Config):
+    """生产环境下的配置"""
+    DEBUG = False
+    # 生产环境下的数据库配置
+    SQLALCHEMY_DATABASE_URI = "mysql://root:123456@127.0.0.1:3306/information4"
+
+
+class TestingConfig(Config):
+    """单元测试环境下的配置"""
+    DEBUG = True
+    TESTING = True
+
+
+config = {
+    "development": DevelopmentConfig,
+    "production": PorductionConfig,
+    "testing": TestingConfig
+}
