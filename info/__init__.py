1b8ab232233a94538f257ad8a0ddf1a35d5f662e
Desperado-lr <suxi1104@gmail.com>
2018-10-17 21:55:31 +0800

    抽取APP的创建逻辑到业务的模块里

---
+++ info/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
+from redis import StrictRedis
+
+from config import Config
+
+app = Flask(__name__)
+# 加载配置
+app.config.from_object(Config)
+
+# 初始化数据库
+db = SQLAlchemy(app)
+# 初始化redis存储对象
+redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+# 开始当前项目 CSRF 保护，只做服务器验证
+CSRFProtect(app)
+# 设置Session 保存到指定位置
+Session(app)
--- manage.py
+++ manage.py
@@ -1,25 +1,8 @@
-from flask import Flask, session
+from flask import session
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
-from flask_session import Session
-from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import CSRFProtect
-from redis import StrictRedis
-from config import Config
 
-app = Flask(__name__)
-
-# 加载配置
-app.config.from_object(Config)
-
-# 初始化数据库
-db = SQLAlchemy(app)
-# 初始化redis存储对象
-redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-# 开始当前项目 CSRF 保护，只做服务器验证
-CSRFProtect(app)
-# 设置Session 保存到指定位置
-Session(app)
+from info import app, db
 
 manager = Manager(app)
 # 将app与db关联
