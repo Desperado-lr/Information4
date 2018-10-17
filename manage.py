14e483a5b1ffd25914ebd10bcc93f24034ebe97f
Desperado-lr <suxi1104@gmail.com>
2018-10-17 18:53:30 +0800

    项目创建 及 数据库配置

---
+++ manage.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Config(object):
+    """项目的配置"""
+    DEBUG = True
+
+    #  为 mysql 添加配置
+    SQLALCHEMY_DATABASE_URI = "mysql://root:123456@127.0.0.1:3306/information4"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+app = Flask(__name__)
+
+# 加载配置
+app.config.from_object(Config)
+
+# 初始化数据库
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def index():
+    return 'index'
+
+
+if __name__ == '__main__':
+    app.run()
