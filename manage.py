8c8a999a7bed886fcc04b71fde8e7c5e70c9fe0d
Desperado-lr <suxi1104@gmail.com>
2018-10-17 23:42:06 +0800

    抽取app创建到函数中 及 配置名字作为参数

--- manage.py
+++ manage.py
@@ -2,7 +2,11 @@ from flask import session
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
-from info import app, db
+from info import create_app, db
+
+# 通过指定的配置名字创建对应配置的app
+# create_app 就类似于 工厂方法
+app = create_app('development')
 
 manager = Manager(app)
 # 将app与db关联
