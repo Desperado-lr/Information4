8430649c2f0eb7f0939f6a3401242f0c53717afd
Desperado-lr <suxi1104@gmail.com>
2018-10-20 19:40:14 +0800

    加载网页小图标

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, current_app
 
 from info import redis_store
 from . import index_blu
@@ -8,4 +8,11 @@ from . import index_blu
 def index():
 
     return render_template("/news/index.html")
+
+
+# 在打开网页的时候 浏览器会默认去请求根路径+favicon.ico作网站标签的小图标
+@index_blu.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
+
 
