06f9dc625f5af60a0266d162a67a9eab79ffde35
Desperado-lr <suxi1104@gmail.com>
2018-10-20 19:21:03 +0800

    根路径的模板渲染

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,3 +1,5 @@
+from flask import render_template
+
 from info import redis_store
 from . import index_blu
 
@@ -5,6 +7,5 @@ from . import index_blu
 @index_blu.route('/')
 def index():
 
-    # 向redis中保存一个值 name itcast
-    redis_store.set("name","Desperado")
-    return 'index'
+    return render_template("/news/index.html")
+
