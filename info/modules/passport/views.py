daf91e53b563f724232b0a406d8d4d0405aa4c4d
Desperado-lr <suxi1104@gmail.com>
2018-10-22 22:07:52 +0800

    登陆的后端逻辑实现

--- info/modules/passport/views.py
+++ info/modules/passport/views.py
@@ -11,6 +11,66 @@ from info.utils.response_code import RET
 from . import passport_blu
 
 
+@passport_blu.route('/login', methods=["POST"])
+def login():
+    """
+    登录
+    1. 获取参数
+    2. 校验参数
+    3. 校验密码是否正确
+    4. 保存用户的登录状态
+    5. 响应
+    :return:
+    """
+
+    # 1. 获取参数
+    params_dict = request.json
+    mobile = params_dict.get("mobile")
+    passport = params_dict.get("passport")
+
+    # 2. 校验参数
+    if not all([mobile, passport]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 校验手机号是否正确
+    if not re.match('1[35678]\\d{9}', mobile):
+        return jsonify(errno=RET.PARAMERR, errmsg="手机号格式不正确")
+
+    # 3. 校验密码是否正确
+    # 先查询出当前是否有指定手机号的用户
+    try:
+        user = User.query.filter(User.mobile == mobile).first()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据查询错误")
+    # 判断用户是否存在
+    if not user:
+        return jsonify(errno=RET.NODATA, errmsg="用户不存在")
+
+    # 校验登录的密码和当前用户的密码是否一致
+    if not user.check_password(passport):
+        return jsonify(errno=RET.PWDERR, errmsg="用户名或者密码错误")
+
+    # 4. 保存用户的登录状态
+    session["user_id"] = user.id
+    session["mobile"] = user.mobile
+    session["nick_name"] = user.nick_name
+
+    # 设置当前用户最后一次登录的时间
+    user.last_login = datetime.now()
+
+    # 如果在视图函数中，对模型身上的属性有修改，那么需要commit到数据库保存
+    # 但是其实可以不用自己去写 db.session.commit(),前提是对SQLAlchemy有过相关配置
+
+    # try:
+    #     db.session.commit()
+    # except Exception as e:
+    #     db.session.rollback()
+    #     current_app.logger.error(e)
+
+    # 5. 响应
+    return jsonify(errno=RET.OK, errmsg="登录成功")
+
 @passport_blu.route('/register', methods=["POST"])
 def register():
     """
@@ -39,7 +99,7 @@ def register():
 
     # 3.取到服务器保存的真实短信验证码内容
     try:
-        real_sms_code = redis_store.get("SMS_" + mobile)
+        real_sms_code = redis_store.get("SMS_" + mobile).decode("utf-8")
     except Exception as e:
         current_app.logger.error(e)
         return jsonify(errno=RET.DBERR, errmsg="查询失败")
@@ -63,18 +123,18 @@ def register():
 
 
     # # 6.添加到数据库
-    # try:
-    #     db.session.add(user)
-    #     db.session.commit()
-    # except Exception as e:
-    #     current_app.logger.error(e)
-    #     db.session.rollback()
-    #     return jsonify(errno=RET.DBERR, errmsg="数据保存失败")
-    #
-    # # 往session 中保存数据便是当前已登陆
-    # session["user_id"] = user.id
-    # session["mobile"] = user.mobile
-    # session["nick_name"] = user.nick_name
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=RET.DBERR, errmsg="数据保存失败")
+
+    # 往session 中保存数据便是当前已登陆
+    session["user_id"] = user.id
+    session["mobile"] = user.mobile
+    session["nick_name"] = user.nick_name
 
     # 7.返回相应
     return jsonify(errno=RET.OK, errmsg="注册成功")
