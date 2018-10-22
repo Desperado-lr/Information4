ff7a233338ebf2217b9d122d05626aee1e7f8bec
Desperado-lr <suxi1104@gmail.com>
2018-10-22 15:23:43 +0800

    发送短信验证码的后端逻辑实现(倒计时)

--- manage.py
+++ manage.py
@@ -7,6 +7,7 @@ from info import create_app, db, models
 # manage.py 是程序启动的入口，之关心启动的相关参数以及内容
 # 不关心具体该如何创建app或者相关业务逻辑
 
+
 # 通过指定的配置名字创建对应配置的app
 # create_app 就类似于 工厂方法
 app = create_app('development')
